a67f04c10b6704c8c991cce5825aa2d2dd4dbaf4
Aymen Jdily <87182808+Aymenjdily@users.noreply.github.com>
2023-06-25 16:05:49 +0100

    create Prompt Succ

--- app/create/page.jsx
+++ app/create/page.jsx
@@ -4,11 +4,14 @@ import { useState } from 'react'
 
 import { useSession } from 'next-auth/react'
 
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 
 import Form from '@components/Form'
 
 const Create = () => {
+    const router = useRouter()
+    const { data:session } = useSession()
+
     const [submitting, setSubmitting] = useState(false)
     const [post, setPost] = useState({
         prompt: '',
@@ -16,7 +19,28 @@ const Create = () => {
     })
 
     const create = async (e) => {
-        
+        e.preventDefault();
+        setSubmitting(true)
+
+        try{
+            const res = await fetch('/api/prompt/new',{
+                method: 'POST',
+                body: JSON.stringify({
+                    prompt: post.prompt,
+                    useId: session?.user.id,
+                    tag: post.tag
+                })
+            })
+
+            if(res.ok){
+                router.push('/')
+            }
+        }
+        catch(error){
+            console.log(error)
+        } finally {
+            setSubmitting(false)
+        }
     }
 
     return (
